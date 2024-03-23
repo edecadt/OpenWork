b567a80af4e5db436f2f968314682386e6edbe07
eziod <ezio.decadt@epitech.eu>
2024-03-23 16:12:49 +0100

    feat: Fetch data api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
 import { auth } from "@/lib/auth";
+import { getData } from "@/lib/utils";
 
 export default async function Home() {
   const session = await auth();
+  const data = await getData();
+  console.log(data);
   return (
     <div>
       <h1>{session?.user ? session.user.email : "Pas co"}</h1>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -5,6 +5,75 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export function test_app_caca() {
-  console.log("test");
+export async function getData() {
+  // Function to get access token
+  const getAccessToken = async () => {
+    const endpoint =
+      "https://entreprise.francetravail.fr/connexion/oauth2/access_token";
+    const realm = "/partenaire";
+    const clientId =
+      "PAR_openwork_a49d3d970583a79501e839028f071d61b3800041039e079dbdee8c9d4ba1f8ee";
+    const clientSecret =
+      "115ebf7ca4bb4f7ea410eae90cef4d98f074b87064ffe3fce3eeec3474884475";
+    const scope = "api_stats-offres-demandes-emploiv1 offresetdemandesemploi";
+
+    const requestBody = new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: clientId,
+      client_secret: clientSecret,
+      scope: scope,
+    });
+
+    const response = await fetch(
+      `${endpoint}?realm=${encodeURIComponent(realm)}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: requestBody,
+      }
+    );
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      throw new Error(errorResponse.error_description);
+    }
+
+    const data = await response.json();
+    return data.access_token;
+  };
+
+  // Fetch data using access token
+  const accessToken = await getAccessToken();
+  console.log(accessToken);
+  const url =
+    "https://api.pole-emploi.io/partenaire/stats-offres-demandes-emploi/v1/indicateur/stat-demandeurs";
+
+  const requestData = {
+    codeTypeTerritoire: "REG",
+    codeTerritoire: "75",
+    codeTypeActivite: "ROME",
+    codeActivite: "A1203",
+    codeTypePeriode: "TRIMESTRE",
+    codeTypeNomenclature: "CATCAND",
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(requestData),
+  });
+
+  if (!response.ok) {
+    console.log(response.status);
+    return [];
+  }
+
+  const data = await response.json();
+  return data;
 }
