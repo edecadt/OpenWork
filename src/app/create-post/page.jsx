dbe174e05329fd5d47ee71725cd508de8c5c8073
eziod <ezio.decadt@epitech.eu>
2024-03-23 20:57:11 +0100

    feat: Update post

--- src/app/create-post/page.jsx
+++ src/app/create-post/page.jsx
@@ -1,15 +1,33 @@
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-export default async function CreatePost() {
-  const session = await auth();
+export default function CreatePost() {
+  const session = useSession();
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [salary, setSalary] = useState(0);
+  const [department, setDepartment] = useState("");
+  const [company, setCompany] = useState("");
+  const [content, setContent] = useState("");
+  const form = useForm();
+
+  console.log(session);
+
   if (!session) {
-    redirect("/auth/login");
-    return;
+    router.push("/auth/login");
+    return null;
   }
+
+  const handleSubmit = () => {};
+
   return (
-    <div>
-      <h1>{session?.user ? session.user.email : "Pas co"}</h1>
-    </div>
+    <div className="flex flex-col items-center justify-center min-h-screen"></div>
   );
 }
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -16,4 +16,21 @@ export const { auth, handlers } = NextAuth({
     signIn: "/auth/login",
     newUser: "/auth/register",
   },
+  callbacks: {
+    session: async ({ session, token }) => {
+      if (session?.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+    jwt: async ({ user, token }) => {
+      if (user) {
+        token.uid = user.id;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
 });
