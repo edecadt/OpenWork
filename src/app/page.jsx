7dc5d2b186872c80ab3aee90d22a2b4e3d57655a
charliinew <charles.ignoux@epitech.eu>
2024-03-23 18:48:57 +0100

    [+]: add icon, name of the site search bar and button to the header

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
 import { auth } from "@/lib/auth";
+import Header from "@/components/header";
+import Under_header from "@/components/under_header";
 
 export default async function Home() {
   const session = await auth();
   return (
     <div>
+      <Header />
+      <Under_header />
       <h1>{session?.user ? session.user.email : "Pas co"}</h1>
     </div>
   );
