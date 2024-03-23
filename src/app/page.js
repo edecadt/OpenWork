ec24f8f2a479d7c35dfad71746fb8b8f31c2e80e
charliinew <charles.ignoux@epitech.eu>
2024-03-23 09:42:44 +0100

    [+]: add the biginning of a header

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import React from 'react';
+import Header from '../components/header';
+
 export default function Home() {
   return (
     <>
-      <h1>OpenWork</h1>
+      <Header/>
     </>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Input } from "./ui/input";
+
+const Header = () => {
+    return (
+      <header className="bg-gray-200 text-Black p-10"> {}
+        <Input/>
+      </header>
+    );
+  };
+
+export default Header;
