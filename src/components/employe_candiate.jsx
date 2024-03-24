e72cae34d98196842919b2466d8a1bef802b7653
lucadekyndt <luca.dekyndt@epitech.eu>
2024-03-24 18:09:54 +0100

    :bug:

--- src/components/employe_candiate.jsx
+++ src/components/employe_candiate.jsx
@@ -4,9 +4,9 @@ import { Button } from "./ui/button";
 
 const Employe_candidate = () => {
   return (
-    <div className="bg-white h-[100px] relative">
+    <div className="bg-white h-[200px] relative">
       <img
-        className="absolute left-[300px] top-[-320px]"
+        className="absolute left-[300px] top-[-220px]"
         src="/candidate.png"
         width={648}
         height={290}
@@ -14,7 +14,7 @@ const Employe_candidate = () => {
       <Link href="/job">
         <Button
           variant="ghost"
-          className="absolute rounded-[4px] left-[350px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
+          className="absolute rounded-[4px] left-[350px] top-[-28px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
         >
           <div href="/job" className="font-semibold text-blue-500">
             Read jobs
@@ -25,7 +25,7 @@ const Employe_candidate = () => {
       </Link>
 
       <img
-        className="absolute left-[972px] top-[-320px]"
+        className="absolute left-[972px] top-[-220px]"
         src="/employe.png"
         width={648}
         height={290}
@@ -33,7 +33,7 @@ const Employe_candidate = () => {
       <Link href="/create-post">
         <Button
           variant="ghost"
-          className="absolute rounded-[4px] left-[1022px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
+          className="absolute rounded-[4px] left-[1022px] top-[-28px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
         >
           <div href="/create-post" className="font-semibold text-blue-500">
             Create post
