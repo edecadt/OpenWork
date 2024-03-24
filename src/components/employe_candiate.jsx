0df78d1296104de65b9da58c88df97a6c0d26bb3
eziod <ezio.decadt@epitech.eu>
2024-03-24 16:41:39 +0100

    fix button link

--- src/components/employe_candiate.jsx
+++ src/components/employe_candiate.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { Button } from "./ui/button";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Employe_candidate = () => {
   return (
@@ -12,28 +11,37 @@ const Employe_candidate = () => {
         width={648}
         height={290}
       />
-      <Button
-        variant="ghost"
-        className="absolute rounded-[4px] left-[350px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
-      >
-        <Link href="/job" className="font-semibold text-blue-500">Read jobs</Link>
-        <div className="w-[20px]" />
-        <ArrowRight className="text-blue-500 w-[20] h-[20]" />
-      </Button>
+      <Link href="/job">
+        <Button
+          variant="ghost"
+          className="absolute rounded-[4px] left-[350px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
+        >
+          <div href="/job" className="font-semibold text-blue-500">
+            Read jobs
+          </div>
+          <div className="w-[20px]" />
+          <ArrowRight className="text-blue-500 w-[20] h-[20]" />
+        </Button>
+      </Link>
+
       <img
         className="absolute left-[972px] top-[-320px]"
         src="/employe.png"
         width={648}
         height={290}
       />
-      <Button
-        variant="ghost"
-        className="absolute rounded-[4px] left-[1022px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
-      >
-        <Link href="/create-post" className="font-semibold text-blue-500">Create post</Link>
-        <div className="w-[20px]" />
-        <ArrowRight className="text-blue-500 w-[20] h-[20]" />
-      </Button>
+      <Link href="/create-post">
+        <Button
+          variant="ghost"
+          className="absolute rounded-[4px] left-[1022px] top-[-128px] w-[188px] h-[49px] bg-white text-blue-500 hover:text-blue-500"
+        >
+          <div href="/create-post" className="font-semibold text-blue-500">
+            Create post
+          </div>
+          <div className="w-[20px]" />
+          <ArrowRight className="text-blue-500 w-[20] h-[20]" />
+        </Button>
+      </Link>
     </div>
   );
 };
